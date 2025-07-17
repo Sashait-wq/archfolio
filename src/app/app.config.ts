ce200ee2ef0eeadf586d56ebef2db46e1006a2fd
Sashait-wq <shevchenoolexandr19@gmail.com>
2025-07-17 17:38:44 +0100

    added a basic service for HTTP requests for simulated assets and deletion of simulated contacts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,11 +5,13 @@ import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(),
     provideStore(),
     provideEffects(),
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
---
+++ src/app/services/base.service.ts
@@ -0,0 +1,25 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, Observable, of, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BaseService {
+  private http = inject(HttpClient);
+  private url = 'assets/mock/';
+
+  private handleError(error: any) {
+    console.error('Error', error);
+    return throwError(() => error);
+  }
+
+  public get(url: string): Observable<any> {
+    return this.http.get<any>(this.url + url).pipe(catchError(this.handleError));
+  }
+
+  public post(data: any): Observable<any> {
+    console.warn('POST does not working with assets/');
+    return of(data).pipe(catchError(this.handleError));
+  }
+}
